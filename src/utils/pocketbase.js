b368d37749bb26756c2c1ad67c99a0a2f23c1550
ndizeyedavid <davidndizeye101@gmail.com>
2025-04-13 16:40:37 +0200

    let

--- src/utils/pocketbase.js
+++ src/utils/pocketbase.js
@@ -2,4 +2,18 @@ import PocketBase from "pocketbase";
 
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 
+// Configure default behaviors
+pb.autoCancellation(false);
+
+// Add default headers for CORS
+pb.beforeSend = function(url, options) {
+    options.headers = {
+        ...options.headers,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    };
+    return { url, options };
+};
+
 export default pb;
